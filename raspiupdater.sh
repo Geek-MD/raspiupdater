020492bbbb19dd80643743bf164ba069c2578330
Edison Montes M <edison.montes@gmail.com>
2023-03-11 16:02:36 -0300

    Update raspiupdater.sh

--- raspiupdater.sh
+++ raspiupdater.sh
@@ -4,8 +4,8 @@ date
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get full-upgrade -y
-sudo apt-get autoclean
-sudo apt-get autoremove
+sudo apt-get autoclean -y
+sudo apt-get autoremove -y
 
 # Update all python packages
 pip list -o --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U
