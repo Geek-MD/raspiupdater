8c72cee28fb8554efb02cef4740aba577957b1a6
Edison Montes M <edison.montes@gmail.com>
2023-12-02 13:15:57 -0300

    Update raspiupdater.sh

--- raspiupdater.sh
+++ raspiupdater.sh
@@ -9,8 +9,3 @@ sudo apt-get upgrade -y
 sudo apt-get full-upgrade -y
 sudo apt-get autoclean -y
 sudo apt-get autoremove -y
-# Update all python packages
-cd /home/emontes/scripts/raspiupdater
-DIR=$(pwd)
-pip freeze > $DIR/requirements.txt
-cat $DIR/requirements.txt | xargs -n 1 pip install -U
